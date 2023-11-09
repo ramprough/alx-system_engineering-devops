139e55c6bd46416b88e222e6922ccd2c59d17451
ramprough <calebmegamind@gmail.com>
2023-11-09 21:48:35 +0000

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet manifest to replace a line in a file on a server
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
